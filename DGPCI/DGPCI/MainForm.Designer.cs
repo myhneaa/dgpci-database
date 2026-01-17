f89e04b13e5daf893213ff4f06d4e1b40636b09f
myhneaa <mihnea.mereuta@s.unibuc.ro>
2026-01-17 16:44:52 +0200

    Added Interface app

---
+++ DGPCI/DGPCI/MainForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace DGPCI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "CATEGORIE",
+            "DETALII_PERMIS",
+            "INMATRICULARE",
+            "ISTORIC_INSTRAINARE",
+            "ITP",
+            "PERMIS",
+            "PERSOANA",
+            "RCA",
+            "SANCTIUNE",
+            "VEHICUL"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(173, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1393, 497);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(122, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(121, 22);
+            this.toolStripMenuItem1.Text = "Modifica";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(121, 22);
+            this.toolStripMenuItem2.Text = "Sterge";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Afiseaza tabel:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 74);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Afiseaza persoanele de sex masculin care detin permis categoria B";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 104);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Afiseaza marca, numarul de masini si puterea medie pentru marcile care au o puter" +
+    "e medie intre 100 si 200 kW";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 58);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Afiseaza detalii despre persoane si permisele detinute";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 358);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 167);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Pentru fiecare tip de combustibil, afiseaza cate masini sunt in total pe acel tip" +
+    ", cea mai puternica masina pe acel tip si media de putere a masinilor de pe acel" +
+    " tip";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1578, 537);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "MainForm";
+            this.Text = "DGPCI";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Click += new System.EventHandler(this.button2_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ DGPCI/DGPCI/MainForm.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DGPCI
+{
+    public partial class MainForm : Form
+    {
+        static string connectionString = "User Id=dgpci; Password=dgpci; Data Source=localhost:1521/xe";
+        OracleConnection connection = new OracleConnection(connectionString);
+        int indexColoanaSelectata = -1;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void incarcaDate(string sql)
+        {
+            try
+            {
+                OracleDataAdapter dataAdapter = new OracleDataAdapter(sql, connection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la incarcarea datelor: " + ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+            }
+        }
+
+        private void dataGridView1_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right && e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                dataGridView1.ClearSelection();
+                dataGridView1.Rows[e.RowIndex].Selected = true;
+                indexColoanaSelectata = e.ColumnIndex;
+                dataGridView1.CurrentCell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string tabelSelectat = comboBox1.SelectedItem.ToString();
+                incarcaDate(@"SELECT *
+                            FROM " + tabelSelectat);
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.SelectedRows.Count == 0 || comboBox1.SelectedItem == null || indexColoanaSelectata == -1)
+            {
+                MessageBox.Show("Celula invalida!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                try
+                {
+                    string tabelSelectat = comboBox1.SelectedItem.ToString();
+                    string numeColoana = dataGridView1.Columns[indexColoanaSelectata].Name;
+                    string valoareVeche = dataGridView1.SelectedRows[0].Cells[indexColoanaSelectata].Value.ToString();
+
+                    string numeColoanaID = "";
+                    switch (tabelSelectat)
+                    {
+                        case "PERSOANA":
+                            numeColoanaID = "ID_PERSOANA";
+                            break;
+                        case "PERMIS":
+                            numeColoanaID = "ID_PERMIS";
+                            break;
+                        case "CATEGORIE":
+                            numeColoanaID = "COD_CATEGORIE";
+                            break;
+                        case "SANCTIUNE":
+                            numeColoanaID = "ID_SANCTIUNE";
+                            break;
+                        case "INMATRICULARE":
+                            numeColoanaID = "ID_INMATRICULARE";
+                            break;
+                        case "RCA":
+                            numeColoanaID = "ID_RCA";
+                            break;
+                        case "ITP":
+                            numeColoanaID = "ID_ITP";
+                            break;
+                        case "DETALII_PERMIS":
+                            numeColoanaID = "ID_PERMIS";
+                            break;
+                        case "ISTORIC_INSTRAINARE":
+                            numeColoanaID = "ID_INSTRAINARE";
+                            break;
+                        default:
+                            MessageBox.Show("Selecteaza un tabel!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                    }
+
+                    string valoareID = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                    if(numeColoana == numeColoanaID)
+                    {
+                        MessageBox.Show("Nu ai voie sa modifici ID-ul (Cheia Primara).", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    string valoareNoua = ShowInputDialog(
+                        "Modifica " + numeColoana + " (Valoare Veche: " + valoareVeche + ")",
+                        "Modificare in " + tabelSelectat,
+                        valoareVeche);
+
+                    if (valoareNoua == "" || valoareNoua == valoareVeche) return;
+
+                    string sql = "UPDATE " + tabelSelectat + " SET " + numeColoana + " = :valoareNoua WHERE " + numeColoanaID + " = :id";
+
+                    using (OracleCommand cmd = new OracleCommand(sql, connection))
+                    {
+                        cmd.Parameters.Add(new OracleParameter("valoareNoua", valoareNoua));
+                        cmd.Parameters.Add(new OracleParameter("id", valoareID));
+
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("A fost modificat cu succes " + numeColoana + ".", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        incarcaDate("SELECT * FROM " + tabelSelectat);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Eroare: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0 & comboBox1.SelectedItem != null)
+            {
+                string tabelSelectat = comboBox1.SelectedItem.ToString();
+                string valoareID = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                string numeColoanaID = "";
+
+                switch (tabelSelectat)
+                {
+                    case "PERSOANA":
+                        numeColoanaID = "ID_PERSOANA";
+                        break;
+                    case "PERMIS":
+                        numeColoanaID = "ID_PERMIS";
+                        break;
+                    case "CATEGORIE":
+                        numeColoanaID = "COD_CATEGORIE";
+                        break;
+                    case "VEHICUL":
+                        numeColoanaID = "SERIE_SASIU";
+                        break;
+                    case "SANCTIUNE":
+                        numeColoanaID = "ID_SANCTIUNE";
+                        break;
+                    case "INMATRICULARE":
+                        numeColoanaID = "ID_INMATRICULARE";
+                        break;
+                    case "RCA":
+                        numeColoanaID = "ID_RCA";
+                        break;
+                    case "ITP":
+                        numeColoanaID = "ID_ITP";
+                        break;
+                    case "DETALII_PERMIS":
+                        numeColoanaID = "ID_PERMIS";
+                        break;
+                    case "ISTORIC_INSTRAINARE":
+                        numeColoanaID = "ID_INSTRAINARE";
+                        break;
+                    default:
+                        MessageBox.Show("Selectati un tabel valid.", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                }
+
+                DialogResult raspuns = MessageBox.Show(
+                    "Sigur stergi inregistrarea cu ID: " + valoareID + " din " + tabelSelectat + "?",
+                    "Confirmare Stergere",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning);
+
+                if (raspuns == DialogResult.Yes)
+                {
+                    try
+                    {
+                        string sql = "DELETE FROM " + tabelSelectat + " WHERE " + numeColoanaID + " = :id";
+
+                        using (OracleCommand cmd = new OracleCommand(sql, connection))
+                        {
+                            cmd.Parameters.Add(new OracleParameter("id", valoareID));
+
+                            int rows = cmd.ExecuteNonQuery();
+                            if (rows > 0)
+                            {
+                                MessageBox.Show("Linia selectata din " + tabelSelectat + " a fost stearsa cu succes.", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                incarcaDate("SELECT * FROM " + tabelSelectat);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Eroare la stergere: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                } 
+            }
+            else
+            {
+                MessageBox.Show("Selecteaza un rand si asigura-te ca ai ales un tabel din meniul de sus.", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                incarcaDate(@"SELECT P.NUME, P.PRENUME, P.JUDET, PC.NUMAR_PERMIS, DP.COD_CATEGORIE 
+                    FROM PERSOANA P
+                    JOIN PERMIS PC ON P.ID_PERSOANA = PC.ID_PERSOANA
+                    JOIN DETALII_PERMIS DP ON PC.ID_PERMIS = DP.ID_PERMIS
+                    WHERE lower(P.COD_PERSOANA) like '1%' and lower(DP.COD_CATEGORIE) like '%b%'");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                incarcaDate(@"SELECT MARCA, COUNT(SERIE_SASIU) NR_MASINI, ROUND(AVG(PUTERE), 2) PUTERE_MEDIE
+                            FROM VEHICUL
+                            GROUP BY MARCA
+                            HAVING AVG(PUTERE) BETWEEN 100 AND 200
+                            ORDER BY PUTERE_MEDIE DESC");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                incarcaDate(@"CREATE OR REPLACE VIEW V_SOFERI_COMPUS AS
+                            SELECT P.NUME, P.PRENUME, P.COD_PERSOANA, PC.NUMAR_PERMIS, PC.DATA_EXPIRARE
+                            FROM PERSOANA P LEFT JOIN PERMIS PC ON P.ID_PERSOANA = PC.ID_PERSOANA");
+                incarcaDate(@"SELECT * FROM V_SOFERI_COMPUS");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                incarcaDate(@"CREATE OR REPLACE VIEW V_STATISTICA_COMBUSTIBIL_COMPLEX AS
+                            SELECT COMBUSTIBIL, COUNT(*) TOTAL_FLOTA, MAX(PUTERE) PUTERE_MAXIMA, ROUND(AVG(PUTERE), 0) PUTERE_MEDIE
+                            FROM VEHICUL
+                            GROUP BY COMBUSTIBIL
+                            ORDER BY TOTAL_FLOTA DESC");
+                incarcaDate(@"SELECT * FROM V_STATISTICA_COMBUSTIBIL_COMPLEX");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            connection.Open();
+        }
+
+        public static string ShowInputDialog(string text, string titlu, string valoareDefaultString = "")
+        {
+            Form prompt = new Form()
+            {
+                Width = 400,
+                Height = 180,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = titlu,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            Label textLabel = new Label() { Left = 20, Top = 20, Text = text, Width = 350 };
+            TextBox inputBox = new TextBox() { Left = 20, Top = 50, Width = 340, Text = valoareDefaultString };
+            Button confirmation = new Button() { Text = "Ok", Left = 250, Width = 100, Top = 90, DialogResult = DialogResult.OK };
+
+            prompt.AcceptButton = confirmation;
+            prompt.Controls.Add(inputBox);
+            prompt.Controls.Add(confirmation);
+
+            return prompt.ShowDialog() == DialogResult.OK ? inputBox.Text : "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
